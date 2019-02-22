c01292f0adb4138fc8e3fbb23bda594fec4369c3
Fredrik Strömbäck <fredde.stromback@gmail.com>
2019-02-22 12:46:24 +0100

    Adds a menu for the game

--- main.js
+++ main.js
@@ -2,12 +2,33 @@
  * The start point for the application.
  */
 
+const readline = require('readline')
+const rl = readline.createInterface(process.stdin, process.stdout)
+
 const WordClass = require('./src/Word.js')
+
 let word = new WordClass('TEST')
 
-function createWord () {
-  word.createLetters()
-  console.log(word.wordArr)
+let menu = 'MENU\nEnter number\n[1] Play Game\n[2] Quit Game\n'
+
+// function createWord () {
+//   word.createLetters()
+//   // console.log(word.wordArr)
+// }
+
+function mainMenu () {
+  rl.question(menu, userInput => {
+    if (userInput === '1') {
+      console.log('Starting game...')
+      rl.close()
+    } else if (userInput === '2') {
+      rl.close()
+    } else {
+      console.log('Invalid choice')
+      mainMenu()
+    }
+  })
 }
 
-createWord()
+mainMenu()
+// createWord()
