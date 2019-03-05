1153d2e70427a771ee843519dfac8413d9680a55
Fredrik Strömbäck <fredde.stromback@gmail.com>
2019-03-05 17:20:52 +0100

    Syntax fixes

--- src/Gamer.js
+++ src/Gamer.js
@@ -1,17 +1,15 @@
 class Gamer {
-    constructor (input) {
-      this.name = input
-      this.mistakeCounter = 0
-      this.correctGuesses = 0
-      this.guessedLetters = []
-    }
-
-    printGamer () {
-      console.log(`Your name is: ${this.name}`)
-      console.log(`You got ${10 - this.mistakeCounter} attemps left.`)
-    }
-    
+  constructor (input) {
+    this.name = input
+    this.mistakeCounter = 0
+    this.correctGuesses = 0
+    this.guessedLetters = []
   }
-  
-  module.exports = Gamer
-  
+
+  printGamer () {
+    console.log(`Your name is: ${this.name}`)
+    console.log(`You got ${10 - this.mistakeCounter} attemps left.`)
+  }
+}
+
+module.exports = Gamer
