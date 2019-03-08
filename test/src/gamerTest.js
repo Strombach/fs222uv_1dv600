0f941744ab86890f0d99aa6969d7c2ba6933d962
Fredrik Strömbäck <fredde.stromback@gmail.com>
2019-03-08 02:42:00 +0100

    Testing to add test functions in a gamerTest suite.

--- test/src/gamerTest.js
+++ test/src/gamerTest.js
@@ -1,3 +1,22 @@
-module.exports = function () {
-  return 'hello'
+const assert = require('chai').assert
+
+function run(test) {
+  describe('Run', function () {
+    it('Should get Test as param', function () {
+      assert.equal(test, 'Test')
+    })
+  })
+}
+
+function test (test) {
+  describe('Test', function () {
+    it('Should get Test as param', function () {
+      assert.equal(test, 'Test1')
+    })
+  })
+}
+
+module.exports = {
+  run,
+  test
 }
--- test/test-runner.js
+++ test/test-runner.js
@@ -1,8 +1,5 @@
 const gamerTest = require('./src/gamerTest')
-const assert = require('chai').assert
+// const assert = require('chai').assert
 
-describe('Gamer', function () {
-  it('Main should return hello', function () {
-    assert.equal(gamerTest(), 'hello')
-  })
-})
+gamerTest.run('Test')
+gamerTest.test('Test')
